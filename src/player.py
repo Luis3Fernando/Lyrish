1c222ffa5a3b2afedc1e74b879ae5438d4410f89
Luis Fernando Chumbes Ramos <luisfernando3chr@gmail.com>
2026-02-18 16:38:53 -0500

    Feat: Agregado de player

---
+++ src/player.py
@@ -0,0 +1,21 @@
+import pygame
+
+class MusicPlayer:
+    def __init__(self):
+        pygame.mixer.init()
+        
+    def player(self, path_file):
+        pygame.mixer.music.load(path_file)
+        pygame.mixer.music.play()
+        
+    def get_time(self):
+        return pygame.mixer.music.get_pos()/1000.0
+    
+    def stop(self):
+        pygame.mixer.music.stop()
+    
+    def pause(self):
+        pygame.mixer.music.pause()
+    
+    def is_playing(self):
+        return pygame.mixer.music.get_busy()
